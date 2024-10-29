fac6751b8d1f2275965374ec853a66d85727aee8
root <root@deployment-server.noobgamer.live>
2024-10-29 07:18:36 +0000

    added dry-run checks

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i inventory/hosts.yml playbook.yml --key-file "/home/runner/.ssh/id_rsa" -vvv
+ansible-playbook -i inventory/hosts.yml playbook.yml --key-file "/home/runner/.ssh/id_rsa" -v
